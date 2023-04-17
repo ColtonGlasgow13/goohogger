1de1a855a4820e56ac0420012c048c4f2a5b9a59
Cole Glasgow <cole@Coles-MacBook-Pro.local>
2023-04-17 10:35:34 -0400

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,4 @@
+document.getElementById("main-image").addEventListener("click", () => {
+    const overlayImageElement = document.getElementById("overlay-image");
+    overlayImageElement.classList.toggle("hidden");
+});
