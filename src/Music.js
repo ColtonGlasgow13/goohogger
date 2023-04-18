db58e4b4ff1b86e2ea44060228e642d0d8cf7456
Cole Glasgow <coleglasgow13@gmail.com>
2023-04-18 14:12:52 -0400

    Make music work for real on click

--- src/Music.js
+++ src/Music.js
@@ -1,12 +1,27 @@
-import audioFile from './Summer-of-Tomfoolery.mp3';
-import React, { Component } from 'react';
+import React, { useRef, useEffect } from 'react';
 
-class Music extends Component {
-    render() {
-        return (
-          <audio src={audioFile} autoPlay loop />
-        );
-      }
-}
+const Music = () => {
+  const audioRef = useRef();
+  const hasPlayed = useRef(false);
+
+  const handlePlay = () => {
+    if (!hasPlayed.current) {
+      audioRef.current.play();
+      hasPlayed.current = true;
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('click', handlePlay);
+
+    return () => {
+      window.removeEventListener('click', handlePlay);
+    };
+  }, []);
+
+  return (
+    <audio ref={audioRef} src="/Summer-of-Tomfoolery.mp3" preload="auto" controls/>
+  );
+};
 
 export default Music;
