6cf0782fccdfc1b0aa0b85d865c41645092503f2
Cole Glasgow <coleglasgow13@gmail.com>
2023-04-18 22:20:49 -0400

    Finalize submission button implementation

--- src/TextInputWidget.js
+++ src/TextInputWidget.js
@@ -4,6 +4,10 @@ import './SubmitButton.css';
 const TextInputWidget = () => {
   const [inputText, setInputText] = useState('');
 
+  const handleSubmit = () => {
+    // Handle submission logic here
+  };
+
   return (
     <div>
       <p>Enter some text:</p>
@@ -12,6 +16,7 @@ const TextInputWidget = () => {
         value={inputText}
         onChange={(e) => setInputText(e.target.value)}
       />
+      <button className="submit-button" onClick={handleSubmit}>Submit</button>
     </div>
   );
 };
