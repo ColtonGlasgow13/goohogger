fac7b20ecc95cfc4303f857a19a5011f85dcd0a5
Cole Glasgow <coleglasgow13@gmail.com>
2023-05-14 19:43:54 -0400

    Implement name widget

--- src/components/TextInputWidget/TextInputWidget.js
+++ src/components/TextInputWidget/TextInputWidget.js
@@ -1,8 +1,25 @@
 // TextInputWidget.js
-import React from 'react';
+import React, { useState } from 'react';
 import './TextInputWidget.css';
+import { putMonsterData } from '../API/API'
 
 const TextInputWidget = () => {
+  const [text, setText] = useState('')
+
+  const handleInputChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    const data = {
+      name: text
+    };
+
+    const idToken = JSON.parse(sessionStorage.getItem('idToken'));
+    putMonsterData(data, idToken);
+  };
+
+
   return (
     <div className="text-input-widget">
       <h3>What is the name of the monster?</h3>
@@ -10,8 +27,10 @@ const TextInputWidget = () => {
         className="text-input"
         type="text"
         placeholder="Enter text here"
+        value={text}
+        onChange={handleInputChange}
       />
-      <button className="submit-button">Submit</button>
+      <button className="submit-button" onClick={handleSubmit}>Submit</button>
     </div>
   );
 };
