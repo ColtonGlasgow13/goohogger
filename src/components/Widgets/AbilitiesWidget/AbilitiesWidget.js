ceaddd9481f128bef6dbdb3c6e570db68ca92470
Cole Glasgow <coleglasgow13@gmail.com>
2023-05-17 12:12:13 -0400

    Add abilities and split them into columns

--- src/components/Widgets/AbilitiesWidget/AbilitiesWidget.js
+++ src/components/Widgets/AbilitiesWidget/AbilitiesWidget.js
@@ -21,33 +21,98 @@ const AbilitiesWidget = () => {
   return (
     <div className="abilities-widget" data-test-id="abilities-widget">
       <h3>Select the abilities of the monster:</h3>
-      <label>
-        <input
-          type="checkbox"
-          name="ability1"
-          checked={values.ability1 || false}
-          onChange={(e) => onChange(e, handleAbilityChange)}
-        />
-        Ability 1
-      </label>
-      <label>
-        <input
-          type="checkbox"
-          name="ability2"
-          checked={values.ability2 || false}
-          onChange={(e) => onChange(e, handleAbilityChange)}
-        />
-        Ability 2
-      </label>
-      <label>
-        <input
-          type="checkbox"
-          name="ability3"
-          checked={values.ability3 || false}
-          onChange={(e) => onChange(e, handleAbilityChange)}
-        />
-        Ability 3
-      </label>
+      <div className="checkbox-group">
+        <label>
+          <input
+            type="checkbox"
+            name="spellcasting"
+            checked={values.spellcasting || false}
+            onChange={(e) => onChange(e, handleAbilityChange)}
+          />
+          Spellcasting
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="areaOfEffect"
+            checked={values.areaOfEffect || false}
+            onChange={(e) => onChange(e, handleAbilityChange)}
+          />
+          Area of Effect
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="hpReduction"
+            checked={values.hpReduction || false}
+            onChange={(e) => onChange(e, handleAbilityChange)}
+          />
+          HP Reduction
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="rangedAttacks"
+            checked={values.rangedWeapons || false}
+            onChange={(e) => onChange(e, handleAbilityChange)}
+          />
+          Ranged Attacks
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="reachAttacks"
+            checked={values.reachAttacks || false}
+            onChange={(e) => onChange(e, handleAbilityChange)}
+          />
+          Reach Attacks
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="recharge"
+            checked={values.recharge || false}
+            onChange={(e) => onChange(e, handleAbilityChange)}
+          />
+          Recharge
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="reachAttacks"
+            checked={values.reachAttacks || false}
+            onChange={(e) => onChange(e, handleAbilityChange)}
+          />
+          Reach Attacks
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="legendary"
+            checked={values.legendary || false}
+            onChange={(e) => onChange(e, handleAbilityChange)}
+          />
+          Legendary
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="lairActions"
+            checked={values.lairActions || false}
+            onChange={(e) => onChange(e, handleAbilityChange)}
+          />
+          Lair Actions
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="reactions"
+            checked={values.reactions || false}
+            onChange={(e) => onChange(e, handleAbilityChange)}
+          />
+          Reactions
+        </label>
+      </div>
       <button className="submit-button" onClick={onSubmit}>Submit</button>
     </div>
   );
